95add0ce6d261f1b086cddcce42ddc701c249ede
PAURO27 <133005312+PAURO27@users.noreply.github.com>
2023-05-11 11:59:27 +0200

    Add files via upload

---
+++ proyecto/plugin-copias.php
@@ -0,0 +1,82 @@
+<?php
+/*
+Plugin Name: Plugin Copias de Seguridad
+Plugin URI: https://example.com/
+Description: Un complemento de copia de seguridad personalizado para WordPress.
+Version: 1.0
+Author: Pau
+*/
+
+// Establecer opciones default
+add_option('backup_frequency', 'daily');
+add_option('backup_type', 'full');
+add_option('backup_location', '/wp-content/backups');
+
+// Programar copias de seguridad (schedule backups)
+function custom_backup_schedule() {
+    $frequency = get_option('backup_frequency');
+    if ($frequency == 'daily') {
+        wp_schedule_event(time(), 'daily', 'custom_backup_event');
+    } elseif ($frequency == 'weekly') {
+        wp_schedule_event(time(), 'weekly', 'custom_backup_event');
+    } elseif ($frequency == 'monthly') {
+        wp_schedule_event(time(), 'monthly', 'custom_backup_event');
+    }
+}
+add_action('admin_init', 'custom_backup_schedule');
+
+// Funcion de la copia de seguridad
+function custom_backup() {
+    //  Tener opciones (tipo y ubicacion) de copia de seguridad
+    $type = get_option('backup_type');
+    $location = get_option('backup_location');
+
+    // Generar nombre de la copia (dia, mes, año)
+    $date = date('Y-m-d');
+    $filename = $type . '-' . $date . '.zip';
+
+    // Backup de archivos y base de datos de WordPress 
+    if ($type == 'full') {
+        // Backup de archivos de WordPress 
+        $zip = new ZipArchive();
+        $zip->open($location . '/' . $filename, ZIPARCHIVE::CREATE | ZIPARCHIVE::OVERWRITE);
+        $rootPath = realpath(dirname(__FILE__) . '/../');
+        $files = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($rootPath),
+            RecursiveIteratorIterator::LEAVES_ONLY
+        );
+        foreach ($files as $name => $file) {
+            if (!$file->isDir()) {
+                $filePath = $file->getRealPath();
+                $relativePath = substr($filePath, strlen($rootPath) + 1);
+                $zip->addFile($filePath, $relativePath);
+            }
+        }
+        $zip->close();
+
+        // Backup de la base de datos WordPress 
+        $backup_file = $location . '/database-' . $date . '.sql';
+        exec("mysqldump --user=" . DB_USER . " --password=" . DB_PASSWORD . " --host=" . DB_HOST . " " . DB_NAME . " > " . $backup_file);
+    } elseif ($type == 'files') {
+        // Backup de archivos de WordPress 
+        $zip = new ZipArchive();
+        $zip->open($location . '/' . $filename, ZIPARCHIVE::CREATE | ZIPARCHIVE::OVERWRITE);
+        $files = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator(ABSPATH),
+            RecursiveIteratorIterator::LEAVES_ONLY
+        );
+        foreach ($files as $name => $file) {
+            if (!$file->isDir()) {
+                $filePath = $file->getRealPath();
+                $relativePath = substr($filePath, strlen(ABSPATH) + 1);
+                $zip->addFile($filePath, $relativePath);
+            }
+        }
+        $zip->close();
+    } elseif ($type == 'database') {
+        // Backup de base de datos de WordPress 
+        $backup_file = $location . '/database-' . $date . '.sql';
+        exec("mysqldump --user=" . DB_USER . " --password=" . DB_PASSWORD . " --host=" . DB_HOST . " " . DB_NAME . " > " . $backup_file);
+    }
+}
+add_action('custom_backup_event', 'custom_backup');
---
+++ proyecto/readme.txt.txt
@@ -0,0 +1,15 @@
+Plugin de Copias de Seguridad Personalizado de WordPress
+Este plugin permite realizar copias de seguridad diarias de los archivos y la base de datos de su sitio web de WordPress. Las copias de seguridad se nombran automáticamente con la fecha de la copia de seguridad y se almacenan en una ubicación especificada.
+
+Instalación
+
+1.Descargar el archivo zip del plugin.
+
+2.Descomprimir el archivo zip en el directorio 
+/wp-content/plugins/
+
+
+3.Active el plugin en el panel de administración de WordPress.
+
+Personalización
+Puede personalizar las opciones de copia de seguridad en el panel de administración de WordPress. Las opciones incluyen la frecuencia de las copias de seguridad, la ubicación de almacenamiento y los formatos de archivo de copia de seguridad.
